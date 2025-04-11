f788e0ee04ac3e0b4058682896a9de401fcd2254
Syedaamna336699 <syedaamnashah816@gmail.com>
2025-04-11 19:49:55 +0500

    assignment to do list app

---
+++ app.py
@@ -0,0 +1,37 @@
+print("hello world")
+print("my name is amna")
+print("keep smiling")
+print("Allah is always with me")
+
+#VARIABLE
+
+A=10
+B=20
+
+print(A)
+print(B) 
+
+#DICTIONARY
+solution=30
+solution=60
+print(30)
+love=True
+print(true)
+
+name = "amna"
+age = 25
+price = 30.99
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+Name = "Saima"
+print(Name)
+
+
+
+
+
+
+
